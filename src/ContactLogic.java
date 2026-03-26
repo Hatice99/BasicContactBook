a7e789321424312b02bf063c4e5e4846f74f6d94
Hatice Taylan <hatice.taylan@rwth-aachen.de>
2026-03-26 14:09:43 +0100

    MVP is done!

--- src/ContactLogic.java
+++ src/ContactLogic.java
@@ -4,39 +4,43 @@ import java.util.Iterator;
 public class ContactLogic {
     private ArrayList<ContactDataModel> contacts = new ArrayList<>();
 
-    public void addContact(String name, String phone_number, String email){
+    public void addContact(String name, String phone_number, String email) {
         ContactDataModel contact = new ContactDataModel(name, phone_number, email);
         contacts.add(contact);
         System.out.print("Contact added successfully!");
     }
 
-    public void deleteContact(int index){
-        if(index <= 0 || index > contacts.size()){
+    public void deleteContact(int index) {
+        if (index < 0 || index >= contacts.size()) {
             System.out.println("Invalid index!");
-        }
-        else{
+        } else {
             ContactDataModel contact = contacts.remove(index);
             System.out.println("Contact deleted:" + contact.getName());
         }
     }
 
-    public void viewContact(){
-        if (contacts.isEmpty()){
+    public void viewContact() {
+        if (contacts.isEmpty()) {
             System.out.println("Contact list is empty");
+            return;
         }
-        
+
         Iterator<ContactDataModel> it = contacts.iterator();
-        while(it.hasNext()){
-            System.out.println(it.next());
+
+        while (it.hasNext()) {
+            ContactDataModel contact = it.next();
+
+            System.out.println("Name:" + contact.getName());
+            System.out.println("Phone Number:" + contact.getPhone_number());
+            System.out.println("Email:" + contact.getEmail());
         }
-        
+
     }
 
-    public void editContact(int index, String name, String phone_number, String mail){
-        if(index <= 0 || index > contacts.size()){
+    public void editContact(int index, String name, String phone_number, String mail) {
+        if (index < 0 || index >= contacts.size()) {
             System.out.println("Invalid index!");
-        }
-        else{
+        } else {
             ContactDataModel new_contact = contacts.get(index);
             new_contact.setName(name);
             new_contact.setPhone_number(phone_number);
@@ -45,6 +49,4 @@ public class ContactLogic {
         }
     }
 
-
-    
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        ContactLogic contact_book = new ContactLogic();
+        Scanner myObj = new Scanner(System.in);
+
+        while(true){
+            System.out.println("""
+            Choose one action from below:
+            1. Add contact
+            2. View contact
+            3. Update contact
+            4. Delete contact
+            5. Exit
+                """);
+
+    int selection_number = myObj.nextInt();
+    myObj.nextLine(); // consume leftover newline
+
+    switch (selection_number) {
+        case 1:
+            System.out.println("Enter the name");
+            String name = myObj.nextLine();
+            System.out.println("Enter the phone number");
+            String phone_number = myObj.nextLine();
+            System.out.println("Enter the email");
+            String email = myObj.nextLine();
+            contact_book.addContact(name, phone_number, email);
+            System.out.println();
+            break;
+        case 2:
+            contact_book.viewContact();
+            break;
+        case 3:
+            System.out.println("Enter the index of the contact:");
+            int index = myObj.nextInt();
+            myObj.nextLine();
+            System.out.println("Enter the name");
+            String new_name = myObj.nextLine();
+            System.out.println("Enter the phone number");
+            String new_phone_number = myObj.nextLine();
+            System.out.println("Enter the email");
+            String new_email = myObj.nextLine();
+            contact_book.editContact(index-1, new_name, new_phone_number, new_email);
+            System.out.println();
+            break;
+        case 4:
+            System.out.println("Enter the index of the contact:");
+            int index_to_delete = myObj.nextInt();
+            myObj.nextLine();
+            contact_book.deleteContact(index_to_delete-1);
+            System.out.println();
+            break;
+        case 5:
+            System.out.println("Bye");
+            myObj.close();
+            return;
+        default:
+            System.out.println("Invalid choice!");
+            System.out.println();
+            break;
+    }
+
+    
+
+        }
+
     }
 }
