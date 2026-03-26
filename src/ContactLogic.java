2c1b2565f8f5dcfc02b3f94a622044088c005de3
Hatice Taylan <hatice.taylan@rwth-aachen.de>
2026-03-26 12:18:14 +0100

    added viewcontact + editcontact methods

--- src/ContactLogic.java
+++ src/ContactLogic.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class ContactLogic {
     private ArrayList<ContactDataModel> contacts = new ArrayList<>();
@@ -16,6 +17,31 @@ public class ContactLogic {
         else{
             ContactDataModel contact = contacts.remove(index);
             System.out.println("Contact deleted:" + contact.getName());
+        }
+    }
+
+    public void viewContact(){
+        if (contacts.isEmpty()){
+            System.out.println("Contact list is empty");
+        }
+        
+        Iterator<ContactDataModel> it = contacts.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+        
+    }
+
+    public void editContact(int index, String name, String phone_number, String mail){
+        if(index <= 0 || index > contacts.size()){
+            System.out.println("Invalid index!");
+        }
+        else{
+            ContactDataModel new_contact = contacts.get(index);
+            new_contact.setName(name);
+            new_contact.setPhone_number(phone_number);
+            new_contact.setEmail(mail);
+            System.out.println("Contact updated successfully!");
         }
     }
 
